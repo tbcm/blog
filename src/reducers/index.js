90ca8ec7afe287f0f9c3b33e470121197634c4d5
milankurjak <milan.nikolic1337@gmail.com>
2019-05-09 15:41:58 +0200

    upload

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import postReducer from './postReducer';
+import userReducer from './usersReducer';
+
+export default combineReducers({
+    posts: postReducer,
+    users: userReducer
+
+});
